02b9a6ca91547f9d3b7fe57240a33cf81e6bf486
collollll <hck2061@naver.com>
2025-09-23 17:54:52 +0900

    last

--- js/custom.js
+++ js/custom.js
@@ -14,14 +14,6 @@ $(function () {
     $("html, body").animate({ scrollTop: target });
   });
 
-  //컨테이너_1st_선 움직임
-  $("#con1 .down").animate({ height: "100%" }, 900);
-  $("#con1 .right").animate({ width: "100%" }, 700);
-
-  //컨테이너_1st_제목
-  $("#con1 h2:first-child").addClass("up");
-  $("#con1 h2:nth-child(2)").addClass("up");
-
   //스크롤 시 움직임
   $(window).on("scroll", function () {
     let scroll = $(this).scrollTop();
@@ -31,7 +23,7 @@ $(function () {
       $("header").addClass("on");
     } else {
       $("header").removeClass("on");
-    } // 이걸 scroll 이벤트 괄호 밖으로 빼면 실행이 안됨 -> 왜?? 그래도 실행돼야 하는거 아님???
+    }
 
     if (scroll >= con1 && scroll < con2) {
       $("#navi li").removeClass("on");
@@ -57,7 +49,23 @@ $(function () {
     } else {
       $("#navi li").removeClass("on");
       $("#navi li").eq(3).addClass("on");
-    } // 조건1은 con1의 범위 내에 있다는 뜻 ->  나머지 조건들도 각각 con2, con3의 범위 내에 있다는 뜻, else는 나머지니까 con4 영역을 의미
+    }
+  });
+
+  //컨테이너_1st_선 움직임
+  $("#con1 .down").animate({ height: "100%" }, 900);
+  $("#con1 .right").animate({ width: "100%" }, 700);
+
+  //컨테이너_1st_제목
+  $("#con1 h2:first-child").addClass("up");
+  $("#con1 h2:nth-child(2)").addClass("up");
+
+  //컨테이너_2nd_이미지
+  $("#con2 img").on("mouseenter", function () {
+    $(this).stop().animate({ height: "115%" });
+  });
+  $("#con2 img").on("mouseleave", function () {
+    $(this).stop().animate({ height: "100%" });
   });
 
   //컨테이너_3rd
@@ -67,20 +75,57 @@ $(function () {
   });
   $("#con3 li").on("mouseenter", function () {
     if (!con3Ready) return;
-
     $("#con3 li").hide();
     $(this).show();
     $(this).stop().animate({ width: "80%" });
-    $(this).find("img").stop().animate({ width: "50%" }, 1000);
-    $(this).find("div").stop().animate({ width: "50%", padding: "20px" }, 1000);
+    $(this).find("img").stop().animate({ width: "50%" }, 500);
+    $(this).find("div").stop().animate({ width: "50%", padding: "20px" }, 500);
   });
   $("#con3 ul").on("mouseleave", function () {
     if (!con3Ready) return;
-
     $("#con3 li").css({ width: "calc(100% / 3)" });
     $("#con3 li").show();
     $(this).find("img").stop().animate({ width: "100%" });
     $(this).find("div").stop().css({ width: "0%", padding: "0px" });
+  });
+
+  // 컨테이너_4th
+  let total = $(".play li").length;
+  let i = 0;
+
+  $(".arr .bar .left").text(i + 1);
+  $(".arr .bar .right").text(total);
+
+  function fade() {
+    $(".arr .bar span")
+      .stop()
+      .animate({ width: (i + 1) * (300 / total) });
+    $(".arr .bar .left").text(i + 1);
+  }
+
+  $(".next").on("click", function () {
+    if (i == total - 1) {
+      i = 0;
+    } else {
+      i++;
+    }
+    $(".play").animate({ "margin-left": "-100%" }, function () {
+      $(".play li:first-child").appendTo(".play");
+      $(".play").css({ "margin-left": "0px" });
+    });
+    fade();
+  });
+
+  $(".prev").on("click", function () {
+    if (i == 0) {
+      i = total - 1;
+    } else {
+      i--;
+    }
+    $(".play li:last-child").prependTo(".play");
+    $(".play").css({ "margin-left": "-100%" });
+    $(".play").animate({ "margin-left": "0px" });
+    fade();
   });
 
   //우측 내비바
@@ -99,59 +144,4 @@ $(function () {
     let target = $("section").eq(i).offset().top;
     $("html, body").animate({ scrollTop: target });
   }
-
-  //
-  //
-  //
-  //
-  //
-  //
-
-  let total = $(".play li").length;
-  let i = 0;
-
-  $(".arr .bar .left").text(i + 1);
-  $(".arr .bar .right").text(total);
-
-  function fade() {
-    $(".play").animate({ "margin-left": "-50%" }, function () {
-      $(".play li:first-child").appendTo(".play");
-      $(".play").css({ "margin-left": "0px" });
-    });
-
-    // $(".play li").fadeOut();
-    // $(".play li").eq(i).fadeIn();
-    // $(".play li").hide();
-    // $(".play li").eq(i).show();
-
-    $(".arr .bar span")
-      .stop()
-      .animate({ width: (i + 1) * (300 / total) });
-    $(".arr .bar .left").text(i + 1);
-  }
-
-  // stop = setInterval(function () {
-  //   if (i == total - 1) {
-  //     i = 0;
-  //   } else {
-  //     i++;
-  //   }
-  // }, 1000);
-
-  $(".next").on("click", function () {
-    if (i == total - 1) {
-      i = 0;
-    } else {
-      i++;
-    }
-    fade();
-  });
-  $(".prev").on("click", function () {
-    if (i == 0) {
-      i = total - 1;
-    } else {
-      i--;
-    }
-    fade();
-  });
 });
